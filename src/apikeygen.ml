6a62bb48b5a3fcab5b7599da766669f2152dac3f
Matt Harrison <matt.anthony.harrison@gmail.com>
2017-02-25 17:33:11 +0000

    Adds terminal doc

--- src/apikeygen.ml
+++ src/apikeygen.ml
@@ -1,11 +1,26 @@
 open Core.Std
 
+let generate_api_key ~service ~cert ~key = ()
+
+let gen =
+  Command.basic
+    ~summary:"Generate an Osilo service client API key."
+    Command.Spec.(
+      empty
+      +> flag "-s" (required string)
+        ~doc:"  Service the client is for."
+      +> flag "-c" (required string)
+        ~doc:"  Path to peer's x509 certificate."
+      +> flag "-k" (required string)
+        ~doc:"  Path to peer's private key file."
+    ) (fun s c k () -> generate_api_key ~service:s ~cert:c ~key:k)
+
 let commands =
   Command.group
     ~summary:"CLI for generating Osilo service client API keys"
-    []
+    [("gen",gen)]
 
-let () = 
+let () =
   Command.run
     ~version:"0.1"
     ~build_info:"osilo-apikeygen"
